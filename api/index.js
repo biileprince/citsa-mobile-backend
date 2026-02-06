5496ee339d0a9fd80f553a65062b87f81792514f
Prince Biile <biileprinceyennuyar5@gmail.com>
2026-02-06 00:38:57 +0000

    chore: remove serverless entry point - switching to traditional hosting

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// Vercel Serverless Function Entry Point
-// This file is at /api/index.js and imports the compiled Express app
-const app = require('../dist/app.js').default;
-
-module.exports = app;
